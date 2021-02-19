ec0b7d60baf5051bdd367eab7470d8544f0584ec
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2021-02-19 17:56:32 +0000

    feat: add translations to static content

--- src/components/globals/Footer.js
+++ src/components/globals/Footer.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useIntl } from "gatsby-plugin-intl";
 
 export default function Footer() {
-    return (
-        <footer className="footer py-3">
-            <div className="container">
-                <div className="row">
-                    <div className="col-10 mx-auto col-md-6 text-white pt-2 text-center text-capitalize">
-                        <p> all rights reserved &copy; {new Date().getFullYear().toString()}</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+  const intl = useIntl();
+
+  return (
+    <footer className="footer py-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 text-white pt-2 text-center text-capitalize">
+            <p>
+              {intl.formatMessage({
+                id: "footer",
+                defaultMessage: "All rights reserved",
+              })}
+              &copy; {new Date().getFullYear().toString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { useIntl } from "gatsby-plugin-intl";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/globals/BackgroundSection";
 import Menu from "../components/menu/Menu";
 
-const MenuPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={data.img.childImageSharp.fluid}
-      title="Menu"
-      subtitle="Our special menu for lovers like you!"
-      styleClass="half-background"
-    />
-    <Menu items={data.menu} />
-  </Layout>
-);
+const MenuPage = ({ data }) => {
+  const intl = useIntl();
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title={intl.formatMessage({
+          id: "menu",
+          defaultMessage: "Menu",
+        })}
+        subtitle={intl.formatMessage({
+          id: "subtitle_menu",
+          defaultMessage: "Our special menu for lovers like you!",
+        })}
+        styleClass="half-background"
+      />
+      <Menu items={data.menu} />
+    </Layout>
+  );
+};
 
 export const query = graphql`
   {
